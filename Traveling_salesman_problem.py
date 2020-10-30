89a4a6cf992df6746ddc383f0df9415dcde04387
MrPoorya <poorya.m.n.b@gmail.com>
2020-10-30 17:34:30 +0330

    Fitness definition was completed

--- Traveling_salesman_problem.py
+++ Traveling_salesman_problem.py
@@ -3,12 +3,31 @@ import csv
 ########## <-Fuctions-> #########
 def Fitness(Sample):
     # Avoid repeating one city twice in the solution
-
+    if (Check_Duplicate_item(Sample)==True):
+        # Sampple has duplicated citis, Its fitness is Zero
+        return 0
     # Avoid setting two citys next to each other
+        for i in range(len(Sample)-2):
+            if (Data[sample[i]][Sample[i+1]] == -1): 
+                # There isn't any direct route between city(i) and city(i+1)
+                return 0
+        if (Data[Sample[-1]][Sample[0]] == -1):
+            return 0
+    
+    # Sample is feasible and we can calculate its fitness:
+    Route_Cost = 0
+    for i in range(len(Sample)-2):
+        Route_Cost += Data[Sample[i]][Sample[i+1]]
+    
+    Route_Cost += Data[Sample[-1]][Sample[0]]
 
-    pass
+    Fit_Value = 1.0 / Route_Cost
+    return Fit_Value
 
-
+def Check_Duplicate_item(Sample):
+    if len(Sample) == len(set(Sample)):
+        return False
+    return True
 
 Data = []
 
